d76d1c112b844a5f37f0deb78e83ad823982d077
Jean Carlos Santana <jsantanaortiz1701@gmail.com>
2024-10-22 12:07:41 -0400

    Se agregaron los IRepositorios y los Repositorios a la clase Program y se conecto la base de datos.

--- Tienda/Program.cs
+++ Tienda/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Tienda.Abstraccion.Repositorio;
+using Tienda.Implementaciones.Repositorio;
+using Tienda.Modelos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TiendaContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnetion")));
+builder.Services.AddScoped<IRepositorioCliente, RepositorioCliente>();
+builder.Services.AddScoped<IRepositorioDetProducto, RepositorioDetProducto>();
+builder.Services.AddScoped<IRepositorioFactura, RepositorioFactura>();
+builder.Services.AddScoped<IRepositorioFoto, RepositorioFoto>();
+builder.Services.AddScoped<IRepositorioProducto, RepositorioProducto>();
 
 var app = builder.Build();
 
